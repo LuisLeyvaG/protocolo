32afc342611f1644761918582a7d688d734f9030
Luis Leyva <leyva.garcia.luis.gerardo@gmail.com>
2022-09-13 14:00:16 -0500

    Primera version de creador de protocolos

---
+++ Protocolo.py
@@ -0,0 +1,56 @@
+
+import sys
+
+lineamientos = []
+nos = []
+
+while True:
+    print("CREA TU PROTOCOLO")
+    eleccion = input("""
+1 - Introducir lineamiento
+2 - Mostrar norma
+3 - Quitar norma
+4 - Borrar el protocolo
+5 - Salir del programa
+Seleccione: """)
+    if eleccion == "1":
+        lineamiento = input("Lineamiento: ")
+        no = float(input("Número de norma: "))
+        lineamientos.append(lineamiento)
+        nos.append(no)
+
+    elif eleccion == "2":
+        if len(lineamientos) <= 0:
+            print("El protocolo está vacío")
+            continue
+
+        print("|             Lineamiento                | Numero de norma   |")
+        indice = 0
+        total = 0
+        while indice < len(lineamientos):
+            lineamiento = lineamientos[indice]
+            no = nos[indice]
+            print("|{:<40}|{:>10.2}|".format(
+                lineamiento, no ))
+            print("-----------------------------------------------------")
+
+            indice += 1
+
+    elif eleccion == "3":
+        nombre_lineamiento = input("Norma a borrar: ")
+        if nombre_lineamiento in lineamientos:
+            indice = lineamientos.index(nombre_lineamiento)
+            del lineamientos[indice]
+            del nos[indice]
+            print(f"Se elimina {nombre_lineamiento}")
+        else:
+            print("El lineamiento no existe")
+
+    elif eleccion == "4":
+        if input("Seguro (s/n): ") == "s":
+            lineamientos = []
+            nos = []
+
+    elif eleccion == "5":
+        if input("Seguro (s/n): ") == "s":
+            sys.exit()
